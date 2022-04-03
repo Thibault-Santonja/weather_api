6d048692909411328df6aa2fd1671ea52371e810
Thibault Santonja <thibault.santonja@gmail.com>
2022-04-03 15:35:12 +0200

    Implement APIs query functions
    - Create test for ,  and
    - Implement the above functions
    - Update Makefile configuration (venv activation error)

--- Makefile
+++ Makefile
@@ -1,36 +1,36 @@
 #!/bin/bash
-.PHONY: test run
+.PHONY: test run synthax
 
 venv:
 	( \
-		python3.10 -m pip install --upgrade pip; \
+		python -m pip install --upgrade pip; \
 		pip install virtualenv; \
-		python3.10 -m venv venv; \
-		source venv/bin/activate; \
+		python -m venv venv; \
+		source venv/Scripts/activate; \
 		pip install -r requirements.txt; \
 	)
 
 ## CI Scripts
 synthax: venv
 	( \
-		source venv/bin/activate; \
+		source venv/Scripts/activate; \
 		pip install flake8; \
 		echo "flake8 analyze :"; \
 		flake8 src --count --exit-zero --max-complexity=10 --max-line-length=120 --show-source --statistics; \
 		echo "flake8 ended."; \
 	)
 
-test: venv run
+test: venv
 	( \
-		source venv/bin/activate; \
-		pip3.10 install coverage; \
-		python3.10 -m coverage run -m unittest; \
+		source venv/Scripts/activate; \
+		pip install coverage; \
+		python -m coverage run -m unittest; \
 		coverage report -m; \
 	)
 
 ## Run script
 run: venv
 	( \
-		source venv/bin/activate; \
-		python3.10 main.py; \
+		source venv/Scripts/activate; \
+		python main.py; \
 	)
---
+++ src/test_meta_weather_api.py
@@ -0,0 +1,54 @@
+from unittest import TestCase
+
+from src.meta_weather_api import query_city_metadata, query_city_forecast, query_api
+from src.config import WEATHER_URL
+
+
+class Test(TestCase):
+    def test_query_city_metadata(self):
+        london_metadata = {
+            'title': 'London',
+            'location_type': 'City',
+            'woeid': 44418,
+            'latt_long': '51.506321,-0.12714'
+        }
+        self.assertEqual(query_city_metadata("London"), london_metadata)
+
+        with self.assertRaises(TypeError):
+            query_city_metadata()
+
+    def test_query_city_forecast(self):
+        city_forecast = query_city_forecast(44418)
+        self.assertIsInstance(city_forecast, list)
+        self.assertGreater(len(city_forecast), 1)
+        self.assertIsInstance(city_forecast[0], dict)
+
+    def test_query_api(self):
+        url = f"{WEATHER_URL}location/search/"
+        parameters = {"query": "Geneva"}
+        expected_response = [
+            {
+                "title": "Geneva",
+                "location_type": "City",
+                "woeid": 782538,
+                "latt_long": "46.208351,6.142700"
+            }
+        ]
+        self.assertEqual(query_api(url, parameters), expected_response)
+
+        url = f"{WEATHER_URL}location/{expected_response[0].get('woeid')}/2022/04/04/"
+        response = query_api(url)
+        self.assertIsInstance(response, list)
+        self.assertIsInstance(response[0], dict)
+        self.assertEqual(response[0].get('applicable_date'), "2022-04-04")
+
+    def test_query_api_wrong_url(self):
+        with self.assertRaises(SystemExit):
+            url = f"{WEATHER_URL}qzkdll/search/"
+            query_api(url)
+
+    def test_query_api_wrong_city(self):
+        with self.assertRaises(SystemExit):
+            url = f"{WEATHER_URL}location/search/"
+            parameters = {"query": "Fake York"}
+            query_api(url, parameters)
