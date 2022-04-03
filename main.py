a03c8f708dfddfb7e862cf9483b280ed1a2c85f1
Thibault Santonja <thibault.santonja@gmail.com>
2022-04-03 16:44:33 +0200

    Implement client functions
    - Create test functions for client
    - Implement client functions based on tests
    - Update

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python3.10
+from src.open_weather_client import resolve_command
 
 
 if __name__ == '__main__':
-    pass
+    resolve_command()
---
+++ src/open_weather_client.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3.10
+import argparse
+
+from src.meta_weather_api import query_city_forecast, query_city_metadata
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+
+def resolve_command() -> str:
+    """
+
+    :return:
+    """
+    arguments = get_command_arguments()
+    city_forecast = get_city_tomorrow_forecast(arguments.get("city"))
+
+    return city_forecast.get('weather_state_name')
+
+
+def get_command_arguments() -> dict:
+    """
+    Returns the weather of a given city.
+
+    :return:
+    """
+    parser = argparse.ArgumentParser(usage="""main.py CITY [OPTIONS]
+        Returns the tomorrow weather of a given city.""")
+    parser.add_argument('city', help="Wanted city's weather.")
+
+    return vars(parser.parse_args())
+
+
+def get_city_tomorrow_forecast(city: str) -> dict:
+    """
+
+    :param city:
+    :return:
+    """
+    city = query_city_metadata(city)
+    city_forecast = query_city_forecast(city.get("woeid"))[0]
+
+    print(f"Tomorrow at {city}, you will have {city_forecast.get('weather_state_name').lower()}.")
+    return city_forecast
